4ac6eb94eff521f50f7eb5a50656548687dca130
Javier <javidx@gmail.com>
2023-02-13 17:23:34 +0200

    Added Tailwind

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -4,7 +4,7 @@ export default {
     public: { url: '/', static: true, dot: true },
     src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv', '@snowpack/plugin-babel'],
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv', '@snowpack/plugin-babel', '@snowpack/plugin-postcss'],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
@@ -17,7 +17,7 @@ export default {
     /* ... */
   },
   devOptions: {
-    /* ... */
+    tailwindConfig: './tailwind.config.js',
   },
   buildOptions: {
     metaUrlPath: 'snowpack'
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+// tailwind.config.js
+module.exports = {
+  mode: 'jit',
+  content: ['./public/*.html', './src/*.{js,jsx,css}'],
+  // specify other options here
+};
