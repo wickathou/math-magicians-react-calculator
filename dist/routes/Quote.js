10aeefa4c74f8c6ce252446d315d52b0801472e2
Javier <javidx@gmail.com>
2023-02-22 13:50:18 +0200

    Updates

--- dist/routes/Quote.js
+++
@@ -1,9 +0,0 @@
-import React from '../../snowpack/pkg/react.js';
-function Quote() {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", {
-    className: "font-bold text-lg"
-  }, "\"Pure mathematics is, in its way, the poetry of logical ideas.\""), /*#__PURE__*/React.createElement("h3", {
-    className: "mt-4"
-  }, "Albert Einstein"));
-}
-export default Quote;
