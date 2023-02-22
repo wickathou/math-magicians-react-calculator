5a5d2403a817decf3b8cbc05f1cdc4c763d87d1f
Javier <javidx@gmail.com>
2023-02-22 13:38:26 +0200

    Updates

---
+++ snowpack/pkg/react.js
@@ -0,0 +1,8 @@
+import { r as react } from './common/index-2759f8a2.js';
+export { r as default } from './common/index-2759f8a2.js';
+import './common/_commonjsHelpers-668e6127.js';
+
+
+
+var useState = react.useState;
+export { useState };
