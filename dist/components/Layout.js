c5794bfa1f9abd500644658d25e02c3240df94af
Javier Hernandez <javier@enddesign.co>
2023-02-22 13:26:09 +0200

    Delete dist directory

--- dist/components/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from '../../snowpack/pkg/react.js';
-import { Outlet } from '../../snowpack/pkg/react-router-dom.js';
-import Navbar from './Navbar.js';
-function Layout() {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Navbar, null), /*#__PURE__*/React.createElement("div", {
-    className: "layout"
-  }, /*#__PURE__*/React.createElement(Outlet, null)));
-}
-export default Layout;
