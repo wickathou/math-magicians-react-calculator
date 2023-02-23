7e82d570ac241228b9ee5c965ba6a02e4d1fe83e
Mudasir Issah <danladi496@gmail.com>
2023-02-23 17:31:33 +0000

    fixed-linter-error

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,24 +1,19 @@
-import operate from "../logic/operate";
+import operate from '../logic/operate';
 
-describe('Operate tests for first value second value and operate value ',() => {
-    it('checking for addition should return 20 when 10 + 10',() => {
-
-        expect(operate(10,10,'+')).toMatch('20');
-    });
-    it('checking for subtraction should return 5 when 10 - 5',() => {
-
-        expect(operate(10,5,'-')).toMatch('5');
-    });
-    it('checking for division should return 2 when 10 / 5',() => {
-
-        expect(operate(10,5,'÷')).toMatch('2');
-    });
-    it('checking for multiply should return 20 when 10 * 2',() => {
-
-        expect(operate(10,2,'x')).toMatch('20');
-    });
-    it('checking for modulo should return 0 when 10 % 2',() => {
-
-        expect(operate(10,2,'%')).toMatch('0');
-    });
+describe('Operate tests for first value second value and operate value ', () => {
+  it('checking for addition should return 20 when 10 + 10', () => {
+    expect(operate(10, 10, '+')).toMatch('20');
+  });
+  it('checking for subtraction should return 5 when 10 - 5', () => {
+    expect(operate(10, 5, '-')).toMatch('5');
+  });
+  it('checking for division should return 2 when 10 / 5', () => {
+    expect(operate(10, 5, '÷')).toMatch('2');
+  });
+  it('checking for multiply should return 20 when 10 * 2', () => {
+    expect(operate(10, 2, 'x')).toMatch('20');
+  });
+  it('checking for modulo should return 0 when 10 % 2', () => {
+    expect(operate(10, 2, '%')).toMatch('0');
+  });
 });
